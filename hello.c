9529f6c9c30503f2109646d15fef0dc712eeb1f6
jyy <15329780701@163.com>
2023-03-12 04:21:27 +0800

    jin first hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include<stadio.h>
 int main(){
-	printf("Hello git Hello git ..");
-	printf("Hello git Hello git ..");
+	printf("Hello git Hello git ..");222
+	printf("Hello git Hello git ..");333
 	return 0;
 }
