759977ea1ae6f50342a2d38996d8e87fe55c1b7e
jyy <15329780701@163.com>
2023-03-12 04:25:54 +0800

    jin.test hello.c

--- hello.c
+++ hello.c
@@ -2,5 +2,8 @@
 int main(){
 	printf("Hello git Hello git ..");222
 	printf("Hello git Hello git ..");333
+	printf("Hello git Hello git ..");
+	prinitf("111111111111111111111");jin.test
+	prinitf("111111111111111111111");
 	return 0;
 }
